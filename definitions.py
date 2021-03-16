0eb19c48f00e8867af5603bdc38d9676213e491a
colby <colbylvickerson@gmail.com>
2021-03-15 22:49:39 -0400

    UPDATE CONFIG FILES: Update configuration files.

--- definitions.py
+++ definitions.py
@@ -1,7 +1,7 @@
 """Contains information on file/directory names/paths"""
 import os
 
-ROOT_DIR = os.path.realpath(__file__)
+ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 
 # Default Directories
 DATA_DIR = os.path.join(
@@ -12,6 +12,3 @@ RAW_DATA_DIR = os.path.join(
     DATA_DIR,
     "raw"
 )
-
-# File Names
-NUMERAI_MODELING_DATA = "numerai_modeling_data"
